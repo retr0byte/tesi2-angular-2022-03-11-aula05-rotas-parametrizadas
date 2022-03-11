5e5d1c7ad2eabc3d170d10fbe910abc19881f9bc
retr0byte <dev.gbcproject@gmail.com>
2022-03-11 20:48:03 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { ListDetailsComponent } from './list-details/list-details.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'list', component: ListComponent },
+      { path: 'list/:index', component: ListDetailsComponent },
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+    ListComponent,
+    ListDetailsComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
