51e304b18b30f7957021e1784c2b6606344025b0
retr0byte <dev.gbcproject@gmail.com>
2022-03-11 21:30:23 -0300

    3 files modified

--- src/app/list-details/list-details.component.ts
+++ src/app/list-details/list-details.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { listData } from '../list';
 
 @Component({
   selector: 'app-list-details',
   templateUrl: './list-details.component.html',
-  styleUrls: ['./list-details.component.css']
+  styleUrls: ['./list-details.component.css'],
 })
 export class ListDetailsComponent implements OnInit {
+  subject;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      this.subject = listData[params.get('index')];
+    });
   }
-
 }
